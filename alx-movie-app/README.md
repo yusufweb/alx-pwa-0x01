e3858d736e81d4b13d76cea5febf7b16134b073e
yusufweb <yusuffajao4@gmail.com>
2025-06-22 15:59:51 +0100

    Moved alx-movie-app into main repo

---
+++ alx-movie-app/README.md
@@ -0,0 +1,19 @@
+## API overview
+
+
+## API version
+
+
+## Available Endpoints
+
+
+## Request and Response Format
+
+
+## Authentication
+
+
+ ## Error Handling
+
+
+ ## Usage Limits and Best Practices
---
+++ alx-movie-app/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["m.media-amazon.com"],
+  },
+};
+
+export default nextConfig;
